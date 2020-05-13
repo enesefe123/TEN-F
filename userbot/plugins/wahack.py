2378bf4dc1f19c4c45dae13d70b13a9866560af9
enesefe123 <63678450+enesefe123@users.noreply.github.com>
2020-05-13 05:52:56 +0300

    Update wahack.py

--- userbot/plugins/wahack.py
+++ userbot/plugins/wahack.py
@@ -32,19 +32,19 @@ async def _(event):
 
             " User online: True\nTelegram access: True\nRead Storage: True ",
 
-            "Hacking... 0%\n[░░░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 20s",
+            "Hacking... 0%\n[░░yarrağı░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 20s",
 
-            "Hacking... 11.07%\n[██░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 18s",
+            "Hacking... 11.07%\n[██░░yemek░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 18s",
 
-            "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 16s",    
+            "Hacking... 20.63%\n[███░░░░░üzeresin░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 16s",    
 
-            "Hacking... 34.42%\n[█████░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 14s",
+            "Hacking... 34.42%\n[█████░░░░yerin de░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 14s",
 
-            "Hacking... 42.17%\n[███████░░░░░░░░░░░░░]\n`Searching for databases`\nETA: 0m, 12s",
+            "Hacking... 42.17%\n[███████░░░olsam░░░░░]\n`Searching for databases`\nETA: 0m, 12s",
 
-            "Hacking... 55.30%\n[█████████░░░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 10s",
+            "Hacking... 55.30%\n[█████████░░░KAÇARDIM░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 10s",
 
-            "Hacking... 64.86%\n[███████████░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 08s",
+            "Hacking... 64.86%\n[███████████░░😈░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 08s",
 
             "Hacking... 74.02%\n[█████████████░░░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 06s",
 
